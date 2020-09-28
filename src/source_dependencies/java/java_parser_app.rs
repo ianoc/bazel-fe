c6c31c60906eb46a7d674bde0fc42ca2f6c33533
Ian O'Connell <ianoc@ianoc.net>
2020-09-28 13:10:58 -0600

    Complete java and scala file parsing

--- src/source_dependencies/java/java_parser_app.rs
+++ src/source_dependencies/java/java_parser_app.rs
@@ -1,45 +1,47 @@
-// use clap::Clap;
+use clap::Clap;
 use std::error::Error;
-// use std::fs;
-// use std::path::PathBuf;
+use std::fs;
+use std::path::PathBuf;
 
-// use bazelfe::scala::parse_imports::ParsedFile;
-// #[derive(Clap, Debug)]
-// #[clap(name = "basic")]
-// struct Opt {
-//     /// Files to process
-//     #[clap(name = "FILE", parse(from_os_str))]
-//     files: Vec<PathBuf>,
-// }
+use bazelfe::source_dependencies::java::parse_file;
+use bazelfe::source_dependencies::SelectorType;
+
+#[derive(Clap, Debug)]
+#[clap(name = "basic")]
+struct Opt {
+    /// Files to process
+    #[clap(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // let opt = Opt::parse();
+    let opt = Opt::parse();
 
-    // for f in opt.files.iter() {
-    //     let content = fs::read_to_string(f)?;
+    for f in opt.files.iter() {
+        let content = fs::read_to_string(f)?;
 
-    //     let parsed_file = ParsedFile::parse(&content).unwrap();
+        let parsed_file = parse_file(&content).unwrap();
 
-    //     for import in parsed_file.imports {
-    //         let suffix = match import.suffix {
-    //             bazelfe::scala::parse_imports::SelectorType::SelectorList(lst) => {
-    //                 let arr = lst
-    //                     .iter()
-    //                     .map(|(a, b)| format!("{}=>{}", a, b.as_ref().unwrap_or(a)))
-    //                     .collect::<Vec<String>>();
+        for import in parsed_file.imports {
+            let suffix = match import.suffix {
+                SelectorType::SelectorList(lst) => {
+                    let arr = lst
+                        .iter()
+                        .map(|(a, b)| format!("{}=>{}", a, b.as_ref().unwrap_or(a)))
+                        .collect::<Vec<String>>();
 
-    //                 arr.join(",")
-    //             }
-    //             bazelfe::scala::parse_imports::SelectorType::WildcardSelector() => "*".to_string(),
-    //             bazelfe::scala::parse_imports::SelectorType::NoSelector() => "".to_string(),
-    //         };
-    //         // println!(
-    //         //     "{}\t{}\t{}",
-    //         //     f.as_path().display(),
-    //         //     import.prefix_section,
-    //         //     suffix
-    //         // );
-    //     }
-    // }
+                    arr.join(",")
+                }
+                SelectorType::WildcardSelector() => "*".to_string(),
+                SelectorType::NoSelector() => "".to_string(),
+            };
+            println!(
+                "{}\t{}\t{}",
+                f.as_path().display(),
+                import.prefix_section,
+                suffix
+            );
+        }
+    }
     Ok(())
 }
